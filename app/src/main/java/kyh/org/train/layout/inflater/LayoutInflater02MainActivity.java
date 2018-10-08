32cd4599ff12092066dd3923382885dc52149f58
sinaburokim8725 <sinaburokim8725@gmail.com>
2018-10-09 04:43:07 +0900

    1.setConteView 메소드가 실행되기전에 뷰들을 참조하면 null 에러가 난다. 그런상황을 재현 시킨것이다.

--- app/src/main/java/kyh/org/train/layout/inflater/LayoutInflater02MainActivity.java
+++ app/src/main/java/kyh/org/train/layout/inflater/LayoutInflater02MainActivity.java
@@ -1,13 +1,26 @@
 package kyh.org.train.layout.inflater;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LayoutInflater02MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //setContentView 이전에 뷰를 참조하므로 실행시 널포이트 에러가 난다.
+        Button button = (Button) findViewById(R.id.button_inflater);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //리소스 폴더에서 화면레이아웃xml파일을 id로 찾아서 메모리에 올린다.
+        //고로 이메쏘드 실행전에 뷰들을 참조할려면 예외상황이 발생한다.
         setContentView(R.layout.activity_layout_inflater02_main);
     }
 }
