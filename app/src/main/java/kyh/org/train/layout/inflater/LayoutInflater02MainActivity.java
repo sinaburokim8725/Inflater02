b3e16b00689c79314ba2af9800fe9700bdc6f916
sinaburokim8725 <sinaburokim8725@gmail.com>
2018-10-09 03:49:18 +0900

    Initial commit 이젠 제대로 이해했다.

---
+++ app/src/main/java/kyh/org/train/layout/inflater/LayoutInflater02MainActivity.java
@@ -0,0 +1,13 @@
+package kyh.org.train.layout.inflater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutInflater02MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_inflater02_main);
+    }
+}
